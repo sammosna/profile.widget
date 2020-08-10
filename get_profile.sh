b5267561483914d6b3a8e224322d323935a99fcb
Samuele <samuele@hgblu.com>
2020-08-10 08:52:52 +0200

    updated profile.widget.zip after merge

--- get_profile.sh
+++ get_profile.sh
@@ -2,4 +2,4 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-dscl . -read /Users/sammosna JPEGPhoto | tail -1 | xxd -r -p > "$DIR/profile.jpg"
+dscl . -read $HOME JPEGPhoto | tail -1 | xxd -r -p > "$DIR/profile.jpg"
